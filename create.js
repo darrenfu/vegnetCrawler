387736e5b3c2a62e04e26fbc1da1ea7d93b4abee
dong chao <dongchao3@lenovo.com>
2014-08-18 19:33:34 +0800

    modify curl config param

--- create.js
+++ create.js
@@ -24,14 +24,11 @@ for (var i = start; i <= end; i++) {
     var config = [
         'url = ' + url,
         'output = ' + output,
-        'proxy = 10.99.60.90:8080',
+//        'proxy = 10.99.60.90:8080',
         'retry = 0',
-        'm = 1',
-        'y = 2',
-        'Y = 1000000' // download speed slower than 100B/s for 20 seconds, abort
-//        '--connect-timeout = 0.1',
-//        '--speed-time = 20',
-//        '--speed-limit = 100' // download speed slower than 100B/s for 20 seconds, abort
+        'max-time = 40', // total time > 40 seconds, abort
+        'speed-time = 20',
+        'speed-limit = 500' // download speed slower than 500B/s for 20 seconds, abort
     ];
 
     var text = config.join('\n');
